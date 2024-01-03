2d0b2afa6e7ba380f76d534ac2181448432bdd38
Florian <hoelken@uni-wuppertal.de>
2024-01-03 12:03:40 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { LeafletMapComponent } from './leaflet-map/leaflet-map.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, LeafletMapComponent, HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'gesundes-tal-demonstrator';
+}
